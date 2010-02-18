35b8a350e42f78854f6c8ef47ec238e65de27541
Mariusz Nowak <mariusz@medikoo.com>
2010-02-18 11:28:38 +0100

    Enable ido-mode via custom settings. Kill buffer created to get elisp path.

--- init.el
+++ init.el
@@ -39,8 +39,10 @@
 ;; Environment variables
 (setq my-dotfiles-dir (file-name-directory (or (buffer-file-name)
 			load-file-name)))
-(setq my-elisp-dir (let
-		((path (buffer-file-name (car (find-function-noselect 'add-hook)))))
+(setq my-elisp-dir (let*
+		((buffer (car (find-function-noselect 'add-hook)))
+			(path (buffer-file-name buffer)))
+		(kill-buffer buffer)
 		(substring path 0 (+ 1 (string-match "/lisp/" path)))))
 (setq my-vendor-dir (concat my-dotfiles-dir "vendor/"))
 (setq my-custom-file-default (concat my-dotfiles-dir "custom.el.default"))
@@ -77,7 +79,6 @@
 (auto-compression-mode t)
 (recentf-mode 1)
 (show-paren-mode 1)
-(ido-mode t)
 (color-theme-initialize)
 
 ;; Default buffer local values
