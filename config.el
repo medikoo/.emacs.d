9eade1b2a5f1d4a2c91fe46ac440f061491f5f4c
Mariusz Nowak <medyk@medikoo.com>
2011-04-07 13:36:54 +0200

    My initial config

---
+++ config.el
@@ -0,0 +1,7 @@
+;; Font
+(set-face-font 'default "-*-Anonymous Pro-*-*-*--11-*-*-*-*-*-*")
+;; (set-face-font 'default "-*-Inconsolata-*-*-*--9-*-*-*-*-*-*")
+;; (set-face-font 'default "-*-Anonymous-*-*-*--9-*-*-*-*-*-*")
+
+(push "/usr/local/git/bin" exec-path)
+(require 'el-kit/html)
