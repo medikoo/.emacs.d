f7ea8c3b6f1ac3e80dbcfbdb3db3999eaa4c3153
Mariusz Nowak <medikoo@medikoo.com>
2011-07-04 11:34:28 +0200

    Better documentation. Cleanup.

---
+++ vendor/my.el
@@ -0,0 +1,17 @@
+(defvar estarter-custom-js-globals
+  (list "ROCHE" "GRONO" "GDG" "GENE" "Λ")
+  "List of possible global names for JS pack")
+
+;; (setq estarter-custom-js-globals (list "ROCHE" "GRONO" "GDG" "GENE" "Λ"))
+
+;;;###autoload
+(defun estarter-custom-js-get-global ()
+  (save-excursion
+    (let ((case-fold-search nil))
+      (or
+        (some (lambda (name)
+            (if (or
+                (search-backward name nil t 1)
+                (search-forward name nil t 1))
+              name)) estarter-custom-js-globals)
+        nil))))
