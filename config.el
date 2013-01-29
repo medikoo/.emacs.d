687cc796c700e5ddbaafb5ec62f1b5b84c2704e1
Mariusz Nowak <mariusz@medikoo.com>
2013-01-29 14:09:56 +0100

    Windows shell fix

--- config.el
+++ config.el
@@ -48,3 +48,13 @@
 		(setq indent-tabs-mode nil)
 		(setq js2-basic-offset 4)))
 (add-hook 'estarter-js2-mode-hook 'estarter-js2-jslintjs)
+
+(defun set-exec-path-from-shell-PATH ()
+	(let ((path-from-shell (replace-regexp-in-string
+					"[ \t\n]*$"
+					""
+					(shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+		(setenv "PATH" path-from-shell)
+		(setq exec-path (split-string path-from-shell path-separator))))
+
+(if window-system (set-exec-path-from-shell-PATH))
