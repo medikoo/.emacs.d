10b779c00e221fa2a9c604b461b7547c368db4d7
Mariusz Nowak <mariusz@medikoo.com>
2013-08-29 10:20:27 +0200

    Ack custom setting

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ack-command "ack -a ")
  '(auto-mode-case-fold t)
  '(auto-save-file-name-transforms (list (list ".*" (concat estarter-dotfiles-dir "auto-save-list/") t)))
  '(backup-directory-alist (list (cons "." (concat estarter-dotfiles-dir "backups/"))))
