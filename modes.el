79c1e359e475eee29b0de73e3ef73d88e3087bfa
Mariusz Nowak <mariusz@medikoo.com>
2013-08-29 10:16:08 +0200

    Process .js files with js-mode

--- modes.el
+++ modes.el
@@ -101,6 +101,8 @@
 (load (concat estarter-vendor-dir "js2"))
 (defvar estarter-js-mode-hook nil
 	"Hook that gest run on activation of `js-mode' but after file locals.")
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js-mode))
 (add-hook 'estarter-js-mode-hook 'estarter-coding-hook-run)
 (ad-activate 'js-indent-line)
 (ad-activate 'js--proper-indentation)
@@ -108,8 +110,6 @@
 ;; js2-mode
 (defvar estarter-js2-mode-hook nil
 	"Hook that gest run on activation of `js2-mode' but after file locals.")
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-hook 'estarter-js2-mode-hook 'estarter-coding-hook-run)
 (add-hook 'estarter-js2-mode-hook 'estarter-js2-tab-width-name)
