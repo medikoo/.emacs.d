4a04c5727cdd8c31892c53871abecc346edfe65e
Mariusz Nowak <medyk@medikoo.com>
2014-12-10 23:03:36 +0100

    Use ag instead of ack

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ack-command "ack -a ")
+ '(ack-command "ag ")
  '(auto-mode-case-fold t)
  '(auto-save-file-name-transforms
 	 (list
